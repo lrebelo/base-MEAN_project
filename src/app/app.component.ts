405d5d886c9d3eb9c4be03b627c868a5b847c549
Luis Rebelo <rebeloml@gmail.com>
2017-03-06 11:10:35 +0000

    Initial Commit for base MEAN project.

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'base MEAN Project';
+  ident = '2017 Luis Rebelo - https://github.com/lrebelo';
+}
